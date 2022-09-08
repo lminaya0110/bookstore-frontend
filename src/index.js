358c3f871c5e416f1aa2f86fd855ff3ad1d71621
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2022-09-08 09:51:49 -0400

    changed port

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+var PORT = process.env.PORT || 5000
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
